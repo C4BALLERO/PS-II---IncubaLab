90143bc2ad531bcd09416a8ca860649a94effbd3
C4BALLERO <09illlanes@gmail.com>
2025-09-22 14:10:35 -0400

    Primer commit con los archivos del proyecto

---
+++ src/assets/views/register.jsx
@@ -0,0 +1,31 @@
+const Register = () => {
+  return (
+    <div>
+      <h1 className="center-text">Crearas una carpeta para el registro que este con el login, lo hice como prueba esto</h1>
+      <p> Register Esta es una prueba para el layout</p>
+      <p>Esta es una prueba para el layout</p>
+      <p>Esta es una prueba para el layout</p>
+      <p>Esta es una prueba para el layout</p>
+      <p>Esta es una prueba para el layout</p>
+      <p>Esta es una prueba para el layout</p>
+      <p>Esta es una prueba para el layout</p>
+      <p>Esta es una prueba para el layout</p>
+      <p>Esta es una prueba para el layout</p>
+      <p>Esta es una prueba para el layout</p>
+      <p>Esta es una prueba para el layout</p>
+      <p>Esta es una prueba para el layout</p>
+      <p>Esta es una prueba para el layout</p>
+      <p>Esta es una prueba para el layout</p>
+      <p>Esta es una prueba para el layout</p>
+      <p>Esta es una prueba para el layout</p>
+      <p>Esta es una prueba para el layout</p>
+      <p>Esta es una prueba para el layout</p>
+      <p>Esta es una prueba para el layout</p>
+      <p>Esta es una prueba para el layout</p>
+      <p>Esta es una prueba para el layout</p>
+      <p>Esta es una prueba para el layout</p>
+    </div>
+  );
+};
+
+export default Register;
